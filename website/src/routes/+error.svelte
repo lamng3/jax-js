155d5910b1e724f246388cf8432d559a8bcdead2
Eric Zhang <ekzhang1@gmail.com>
2025-02-28 16:16:45 -0500

    Add 404 not found page

---
+++ website/src/routes/+error.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { base } from "$app/paths";
+  import { page } from "$app/state";
+
+  const error = $derived(page.error!);
+</script>
+
+<svelte:head>
+  <title>{page.status === 404 ? "Not Found" : "Error"} | jax-js</title>
+</svelte:head>
+
+<div class="min-h-screen flex sm:items-center sm:justify-center px-4 py-8">
+  <div class="flex flex-col sm:flex-row gap-x-8 gap-y-6">
+    <a href="{base}/" class="block text-2xl font-medium sm:py-1">jax-js</a>
+    <div class="sm:border-l sm:border-gray-300 sm:pl-4 sm:py-1">
+      <h1 class="text-3xl mb-5">{page.status} {error.message}</h1>
+      <p class="text-lg">
+        {page.status === 404
+          ? "We couldn't find what you were looking for!"
+          : "Oops! Something went wrong."}
+      </p>
+      <p class="text-lg">
+        <a href="{base}/" class="underline">Return home</a>
+      </p>
+    </div>
+  </div>
+</div>
