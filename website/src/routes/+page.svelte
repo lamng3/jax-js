9b12a6a56501fd298e38f0eb44e3c32fe9f8ddc5
Eric Zhang <ekzhang1@gmail.com>
2025-04-12 16:42:24 -0400

    Edit front page

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -21,13 +21,13 @@
   <pre class="mb-6 text-center"><code>npm install @jax-js/core</code></pre>
 
   <p class="mb-6">
-    <code>jax-js</code> brings high-performance arrays and
+    <code>jax-js</code> brings high-performance GPUs and
     <a
       class="link"
       target="_blank"
       href="https://en.wikipedia.org/wiki/Automatic_differentiation">autograd</a
-    > to JavaScript. So you can explore neural networks (AI), statistics, image processing,
-    and computational science.
+    > to JavaScript. So you can do neural networks, statistics, image processing,
+    graphics, and all sorts of numerical computing.
   </p>
 
   <div class="mb-6 grid grid-cols-2 sm:grid-cols-4 gap-2 sm:gap-4">
@@ -47,14 +47,14 @@
   </div>
 
   <p class="mb-6">
-    Like the Python libraries it's inspired by (NumPy, PyTorch, JAX), <code
+    Like the Python libraries it's inspired by (JAX, NumPy, PyTorch), <code
       >jax-js</code
     >
     is carefully hand-optimized, including a JIT compiler and scheduler for GPU kernels.
-    But it's also
+    It's also
     <strong>extremely simple and portable</strong>, running on Chrome, Firefox,
     Safari, iOS, and Android. On each platform, the compiler generates
-    specialized kernels for a variety of hardware.
+    specialized kernels depending on the hardware.
   </p>
 
   <div class="mb-6 border h-48 flex items-center justify-center square-grid">
@@ -66,24 +66,23 @@
 
   <p class="mb-6">
     How is this possible? In short: we compile operations to shaders running in
-    WebAssembly, WebGL, and WebGPU on modern browsers. With some performance
-    engineering, we can get close to native speed, despite being a featherweight
-    cross-platform API.
+    WebAssembly, WebGL, and WebGPU on modern browsers. The built-in compiler
+    generates very fast kernels, faster than hand-tuned neural network libraries
+    like <a href="https://www.tensorflow.org/" class="link">TensorFlow.js</a> at
+    running their own models.
   </p>
 
   <p class="mb-6">
     <span class="italic"
-      >XXX (Performance benchmarks coming soon—though expect <code>jax-js</code>
-      to be faster than TensorFlow.js even at running TensorFlow.js models due to
-      JIT and kernel efficiency.)</span
+      >XXX Performance benchmarks coming soon—though see
+      <a href="{base}/matmul" class="link">here</a> for some initial results.</span
     >
   </p>
 
   <p class="mb-6">
-    Running GPU code should be as easy as making a website. With <code
-      >jax-js</code
-    >, you can bring neural networks to your users in the browser, with zero
-    installs or setup.
+    But although it's very good at ML, <code>jax-js</code> isn't just intended
+    for deep learning. It's a general <em>numerical computing library</em>
+    like JAX and NumPy, with GPUs and autograd attached.
   </p>
 
   <p>
